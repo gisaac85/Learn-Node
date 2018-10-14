8fd21e1bc9f631865b6e81a76d6f268192820926
Gaorieh Isaac <gaorieh@hotmail.com>
2018-10-15 00:55:49 +0200

    add pug files

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -4,6 +4,24 @@ const router = express.Router();
 // Do work here
 router.get('/', (req, res) => {
   res.send('Hey! It works!');
+
 });
+
+router.get('/home', (req, res) => {
+  const wes = { name: 'gaorieh', age: '33' };
+  //res.json(wes);
+  //res.send('Hey! Iam in Home Page');
+  //res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello');
+
+});
+
+router.get('/home1/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
+
+const myCode = "<html><head></head><body><p>Hello</p></body></html>"
 
 module.exports = router;
